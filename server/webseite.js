a27e3048f8538eefd04602704433a6b963770683
Maurice <heroldmaurice@gmail.com>
2025-04-29 15:54:29 +0200

    a

--- server/webseite.js
+++ server/webseite.js
@@ -4,14 +4,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 9090;
 
-const staticPath = path.join(__dirname, '../warenwirtschaftssystem/dist/warenwirtschaftssystem/browser');
+// Sauberer statischer Pfad + URL-Root definieren
+const staticPath = '/home/Ceramica/warenwirtschaftssystem/dist/warenwirtschaftssystem/browser';
+app.use('/', express.static(staticPath));
 
-app.use(express.static(staticPath));
-
+// Redirect alle anderen Anfragen auf index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(staticPath, 'index.html'));
 });
 
+// Server starten
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server läuft auf http://0.0.0.0:${port}`);
 });
