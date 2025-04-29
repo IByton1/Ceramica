7095fc1384c154927efdead0a0355cafe690de2f
Maurice <heroldmaurice@gmail.com>
2025-04-29 15:43:46 +0200

    feat: add Express server to serve static files and handle routing

---
+++ server/webseite.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 9090;
+
+// Serve static files from the browser directory
+app.use(express.static('/home/Ceramica/warenwirtschaftssystem/dist/warenwirtschaftssystem/browser'));
+
+// Redirect all requests to index.html
+app.get('*', (req, res) => {
+    res.sendFile('/home/Ceramica/warenwirtschaftssystem/dist/warenwirtschaftssystem/browser/index.html');
+});
+
+// Start the server
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running and accessible on http://0.0.0.0:${port}`);
+});
