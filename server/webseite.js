1add1f980245624661ac93d2f8368d7c9b140bf8
Maurice <heroldmaurice@gmail.com>
2025-04-29 15:52:19 +0200

    fix: update static file serving path and improve request handling

--- server/webseite.js
+++ server/webseite.js
@@ -4,15 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 9090;
 
-// Serve static files from the browser directory
-app.use(express.static('/home/Ceramica/warenwirtschaftssystem/dist/warenwirtschaftssystem/browser'));
+const staticPath = path.join(__dirname, '../warenwirtschaftssystem/dist/warenwirtschaftssystem/browser');
 
-// Redirect all requests to index.html
+app.use(express.static(staticPath));
+
 app.get('*', (req, res) => {
-    res.sendFile('/home/Ceramica/warenwirtschaftssystem/dist/warenwirtschaftssystem/browser/index.html');
+    res.sendFile(path.join(staticPath, 'index.html'));
 });
 
-// Start the server
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running and accessible on http://0.0.0.0:${port}`);
+    console.log(`Server läuft auf http://0.0.0.0:${port}`);
 });
