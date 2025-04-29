f1fcce45a5ad37dfa8b675a554841026b87dc307
Maurice <heroldmaurice@gmail.com>
2025-04-29 16:09:10 +0200

    update server port and static file paths for deployment

--- server/webseite.js
+++ server/webseite.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9090;
 
 // Serve static files from the browser directory
-app.use(express.static('/root/Warenwirtschaftssystem/frontend-Automaten/dist/frontend-automaten/browser'));
+app.use(express.static('/home/Ceramica/warenwirtschaftssystem/dist/frontend-automaten/browser'));
 
 // Redirect all requests to index.html
 app.get('*', (req, res) => {
-    res.sendFile('/root/Warenwirtschaftssystem/frontend-Automaten/dist/frontend-automaten/browser/index.html');
+    res.sendFile('/home/Ceramica/warenwirtschaftssystem/dist/frontend-automaten/browser/index.html');
 });
 
 // Start the server
